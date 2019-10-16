d34d8c32e7e0a34ea696281647f7d2f3ade3c7cc
FCF5646448 <2998000457@qq.com>
2019-10-16 23:48:21 +0800

    完成基数排序

--- C数据结构与算法学习/C数据结构与算法学习/链表/examples/RadixSort.c
+++ C数据结构与算法学习/C数据结构与算法学习/链表/examples/RadixSort.c
@@ -8,8 +8,18 @@
 
 #include "RadixSort.h"
 
-//
+/* 总结：
+ 1、指针的含义不是特别清晰，通篇代码里都是在移动指针的位置
+ 2、每个链表都有一个header
+ 3、创建链表只是创建一个带有默认值的单结点。
+ 4、整个排序的规则是：
+        1）先把所有的数据用链表串起来；然后生成一个从0~9的基数桶，每个桶都放一个链表；
+        2）然后先从最低位开始，将该位数对应的数据放到对应的桶链表里(链表自然就从小到大排序);
+        3）遍历完第一遍后，将所有数据按基数顺序又重新串联起来；
+        4）进行进一位遍历，重复2、3步骤。
+ */
 
+//
 struct Node {
     ElementType Element;
     PNode Next;
@@ -36,39 +46,35 @@ void insert(List L, ElementType element) {
     }
     tempNode->Element = element;
     tempNode->Next = NULL;
-//    while (NULL != P->Next) {
-//        P = P->Next;
-//    }
     L->Next = tempNode;
 }
 
 // 计算 当前位数上的余数, pos 代表位数，0是个位、1是十、2是百... , radix代表当前基数10
 int get_num_pos(int num, int pos, int radix) {
-    int temp = 1;
-    for (int i = 0; i<pos-1; i++) {
-        temp *= 10;
-    }
-    return (num/temp)%radix;
+    int temp = 1, i;
+    for (i = 0; i < pos - 1; i++)
+        temp *= radix;
+    return (num / temp) % radix;
 }
 
 void radixSort(List list,int radix,int pos_len) {
-    PNode P,n;
+    PNode P;
     List assignArr[radix];
     //先初始化一个数组，数组的每个元素是一个空链表
     for (int i = 0; i < radix; i++) {
         assignArr[i] = createLink();
     }
     
-    for (int i=0; i<pos_len; i++) {
+    for (int i=1; i<=pos_len; i++) {
         P = list;
+        // 将链表里对应的 元素放到对应数组下标对应的链表里面 无需保留原来链表
         while (P->Next != NULL) {
-            // 将链表里对应的 元素放到对应数组下标对应的链表里面
             // 取一个node
-//            PNode n;
+            PNode n;
             n = P->Next;
-            P = P->Next;
+            P->Next = n->Next; //直接丢弃这个结点。
             n->Next = NULL;
-            int result = get_num_pos(n->Element, pos_len, radix);
+            int result = get_num_pos(n->Element, i, radix);
             //
             PNode elementLink = assignArr[result];
             
@@ -76,15 +82,9 @@ void radixSort(List list,int radix,int pos_len) {
                 elementLink = elementLink->Next;
             }
             elementLink->Next = n;
-            
         }
         
-//        printf("第%d次分配\n", i);
-//        for (int j = 0; j < radix; j++) {
-//            printf("N-%d\t", j);
-//        }
-        
-        //将第一次整理好的数据重新整理成链表
+        //将第一次整理好的数据重新整理成 链表
         P = list;
         
         for (int i = 0; i<radix; i++) {
@@ -94,7 +94,7 @@ void radixSort(List list,int radix,int pos_len) {
             while (NULL != elementLink->Next) {
                 PNode n;
                 n = elementLink->Next; //取出结点
-                elementLink = elementLink->Next; //指针顺移
+                elementLink->Next = n->Next; //链表直接截断已取出的结点
                 n->Next = NULL; //将结点独立
                 
                 P->Next = n; //将结点付给P
@@ -109,7 +109,7 @@ void radixSort(List list,int radix,int pos_len) {
 void testRadix(void)
 {
     PNode source, P;
-    int arr[N] = {64, 8, 216, 512, 27, 729, 0, 1, 343, 125};
+    int arr[N] = {64, 8, 8, 512, 127, 729, 7, 1, 343, 125};
     int i;
     int max = 1;
     for (i = 0; i <= POS_Len - 1; i++)
@@ -129,13 +129,13 @@ void testRadix(void)
     }
     
     radixSort(source, Radix, POS_Len);
-    printf("结果\n");
-    P = source;
-    while (NULL != P->Next) {
-        printf("%d",P->Element);
+    printf("\n结果: ");
+    P = source->Next;
+    while (NULL != P ) {
+        printf("%d ",P->Element);
         P = P->Next;
     }
-//    print(source);
+    printf("\n");
 }
 
 
