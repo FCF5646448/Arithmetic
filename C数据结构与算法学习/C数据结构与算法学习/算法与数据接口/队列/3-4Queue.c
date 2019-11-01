1ce9057c307abf31460a9d0997842a9c5dabb6dc
fengcaifan <2998000457@qq.com>
2019-11-01 09:45:20 +0800

    开始队列学习

--- C数据结构与算法学习/C数据结构与算法学习/算法与数据接口/队列/3-4Queue.c
+++ C数据结构与算法学习/C数据结构与算法学习/算法与数据接口/队列/3-4Queue.c
@@ -7,3 +7,58 @@
 //
 
 #include "3-4Queue.h"
+#include <stdlib.h>
+
+int IsEmpty(Queue Q) {
+    return Q->Capacity == 0;
+}
+
+int IsFull(Queue Q) {
+    return Q->Size == Q->Capacity;
+}
+
+Queue CreateQueue(int MaxElements) {
+    Queue q;
+    q = malloc(sizeof(struct Queuerecord));
+    if (q == NULL) {
+        return NULL;
+    }
+    q->Capacity = MaxElements;
+    q->Rear = 0;
+    q->Front = 0;
+    q->Size = 0;
+    q->Array = malloc(sizeof(ElementType)*MaxElements);
+    return q;
+}
+
+void DisposeQueue(Queue Q) {
+    Q->Capacity = 0;
+    Q->Front = 0;
+    Q->Rear = 0;
+    Q->Size = 0;
+    free(Q->Array);
+    free(Q);
+}
+
+void MakeEmpty(Queue Q) {
+    Q->Capacity = 0;
+    Q->Front = 0;
+    Q->Rear = 0;
+    Q->Size = 0;
+}
+
+void Enqueue(ElementType X,Queue Q) {
+    
+}
+
+ElementType Front(Queue Q) {
+    
+}
+
+void Dequeue(Queue Q) {
+    
+}
+
+ElementType FrontAndDequeue(Queue Q) {
+    
+}
--- C数据结构与算法学习/C数据结构与算法学习/算法与数据接口/队列/3-4Queue.h
+++ C数据结构与算法学习/C数据结构与算法学习/算法与数据接口/队列/3-4Queue.h
@@ -10,5 +10,26 @@
 #define __4Queue_h
 
 #include <stdio.h>
+typedef int ElementType;
+typedef struct Queuerecord * Queue;
+
+struct Queuerecord {
+    int Capacity;
+    int Front;
+    int Rear;
+    int Size;
+    ElementType *Array;
+};
+
+int IsEmpty(Queue Q);
+int IsFull(Queue Q);
+Queue CreateQueue(int MaxElements);
+void DisposeQueue(Queue Q);
+void MakeEmpty(Queue Q);
+void Enqueue(ElementType X,Queue Q);
+ElementType Front(Queue Q);
+void Dequeue(Queue Q);
+ElementType FrontAndDequeue(Queue Q);
+
 
 #endif /* __4Queue_h */
