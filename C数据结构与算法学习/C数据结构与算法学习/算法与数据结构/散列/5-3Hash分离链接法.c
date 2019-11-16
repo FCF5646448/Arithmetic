465c40c43ad779f4bcfbd0a5c8bebb15a9dba954
fengcaifan <2998000457@qq.com>
2019-11-16 16:17:18 +0800

    完成散列表分离链接法

--- C数据结构与算法学习/C数据结构与算法学习/算法与数据结构/散列/5-3Hash分离链接法.c
+++ C数据结构与算法学习/C数据结构与算法学习/算法与数据结构/散列/5-3Hash分离链接法.c
@@ -7,3 +7,96 @@
 //
 
 #include "5-3Hash分离链接法.h"
+#include <stdlib.h>
+
+/*
+ hash表是包含一个大小和一个连续内存的list，list存的是指针，指针指向对应key值的链表
+ */
+HashTable HashInitializeTable(int tableSize) {
+    
+    HashTable H;
+    
+    if (tableSize < 0) {
+        return NULL;
+    }
+    
+    H = malloc(sizeof(struct HSTable)); //其实也只是一个指针
+    if (NULL == H) {
+        return NULL;
+    }
+    H->tablesize = tableSize;
+    H->theLists = malloc(sizeof(struct HashListNode * )*tableSize);
+    if (NULL == H->theLists) {
+        printf("out of space");
+        return NULL;
+    }
+    
+    for (int i = 0; i<H->tablesize; i++) {
+        H->theLists[i] = malloc(sizeof(struct HashListNode));
+        if (NULL == H->theLists[i]) {
+            printf("out of space");
+            return NULL;
+        }
+        H->theLists[i]->next = NULL;
+    }
+    
+    
+    return H;
+}
+
+void HashDestoryTable(HashTable H) {
+    if (NULL == H) {
+        return;
+    }
+    
+    if (NULL != H->theLists) {
+        for (int i=0; i<H->tablesize; i++) {
+            free(H->theLists[i]);
+        }
+        free(H->theLists);
+    }
+    H->tablesize = 0;
+    free(H);
+}
+
+//先通过散列函数获取到元素处于当前散列表的位置，然后再拿到散列表里的链表，最后在链表中取出所要找的元素
+HashPosition HashFind(HashElementType val, HashTable H) {
+    HashPosition P;
+    HashList L;
+    L = H->theLists[(val%H->tablesize)]; //这里应该是具体的散列函数，这里暂时用mod代替
+    P = L->next;
+    while (NULL != P && P->val != val) {
+        P = P->next;
+    }
+    return P;
+}
+
+
+void HashInsert(HashElementType val,HashTable H) {
+    HashPosition P;
+    P = HashFind(val, H);
+    if (P == NULL) {
+        // 确保不存在后再插入
+        HashList temp = malloc(sizeof(struct HashListNode));
+        if (NULL == temp) {
+            return;
+        }
+        
+        //找到已经存在的链表
+        HashList L = H->theLists[(val%H->tablesize)];
+        
+        temp->next = L->next;
+        temp->val = val;
+        
+        L->next = temp;
+        
+    }
+}
+
+
+HashElementType HashRetrieve(HashPosition P) {
+    return 0;
+}
+
+
+
--- C数据结构与算法学习/C数据结构与算法学习/算法与数据结构/散列/5-3Hash分离链接法.h
+++ C数据结构与算法学习/C数据结构与算法学习/算法与数据结构/散列/5-3Hash分离链接法.h
@@ -11,4 +11,37 @@
 
 #include <stdio.h>
 
+/*
+ 散列表的分离链接法:解决冲突的方式是将key相同的节点通过一个链表存起来。
+ */
+typedef int HashElementType;
+struct HashListNode;
+typedef struct HashListNode * HashList;
+typedef HashList HashPosition;
+
+struct HashTable;
+typedef struct HSTable * HashTable;
+
+struct HashListNode {
+    HashElementType val;
+    HashPosition next;
+};
+
+struct HSTable {
+    int tablesize;
+    HashList * theLists; //注意这是个指针类型
+};
+
+
+HashTable HashInitializeTable(int tableSize);
+
+void HashDestoryTable(HashTable H);
+
+HashPosition HashFind(HashElementType val, HashTable H);
+
+void HashInsert(HashElementType val,HashTable H);
+
+HashElementType HashRetrieve(HashPosition P);
+
+
 #endif /* __3Hash______h */
