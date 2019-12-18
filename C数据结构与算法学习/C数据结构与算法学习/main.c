624f286098ae70b8755c9a8d23052ac39757cda6
fengcaifan <2998000457@qq.com>
2019-12-18 18:58:05 +0800

    开始不畏艰难，学习图相关算法哦

--- C数据结构与算法学习/C数据结构与算法学习/main.c
+++ C数据结构与算法学习/C数据结构与算法学习/main.c
@@ -20,11 +20,12 @@
 #include "LeetCodeTreeExamples.h"
 
 #include "LeetCodeArrExamples.h"
+#include "图邻接矩阵表示.h"
+
+
 
 #include "Sort.h"
-
 #include "解密QQ号_队列.h"
-
 #include "解密回文_栈.h"
 #include "小猫钓鱼.h"
 #include "深度优先遍历.h"
@@ -82,6 +83,7 @@ int main(int argc, const char * argv[]) {
     
     
     
+    testCreatG();
     
     
     testAHA();
@@ -104,5 +106,5 @@ void testAHA() {
 //    dapaitest();
     
 //    testDfs();
-    ahTestLink();
+//    ahTestLink();
 }
---
+++ C数据结构与算法学习/C数据结构与算法学习/算法与数据结构/9图论/图的表示/图邻接矩阵表示.h
@@ -0,0 +1,16 @@
+//
+//  图邻接矩阵表示.h
+//  C数据结构与算法学习
+//
+//  Created by 冯才凡 on 2019/12/18.
+//  Copyright © 2019 冯才凡. All rights reserved.
+//
+
+#ifndef ________h
+#define ________h
+
+#include <stdio.h>
+
+void testCreatG();
+
+#endif /* ________h */
