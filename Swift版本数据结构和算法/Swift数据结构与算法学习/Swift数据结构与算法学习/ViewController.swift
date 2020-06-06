9a1693b105a9b16ad5dc7f95b0fdef13e1bc7acd
FCF5646448 <2998000457@qq.com>
2020-06-06 15:17:05 +0800

    单链表翻转

--- Swift版本数据结构和算法/Swift数据结构与算法学习/Swift数据结构与算法学习/ViewController.swift
+++ Swift版本数据结构和算法/Swift数据结构与算法学习/Swift数据结构与算法学习/ViewController.swift
@@ -13,7 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Solution().testQuickSort()
+//        Solution().testQuickSort()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        testhuiwen()
+    }
+    
+    
+    func testhuiwen() {
+        let h = ListNode(1)
+        let f = ListNode(4)
+        let s = ListNode(-1)
+        let t = ListNode(-1)
+        let t1 = ListNode(4)
+        let s1 = ListNode(1)
+        let f1 = ListNode(2)
+        let h1 = ListNode(1)
+        
+        h.next = f
+        f.next = s
+        s.next = t
+        t.next = t1
+        t1.next = s1
+//        s.next = f1
+//        f1.next = h1
+        
+        if Solution().isPalindrome(h) == true {
+            print("是回文链表")
+        }else{
+            print("不是回文链表")
+        }
+        
     }
 
 
